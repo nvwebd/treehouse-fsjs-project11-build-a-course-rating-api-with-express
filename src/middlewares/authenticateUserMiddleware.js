ffd7a9e3fd2e13a3c50590ad562c3aa05dc30ba5
Nejc Vukovic <nejc.vukovic@nvwebd.com>
2019-03-07 01:08:14 +0100

    add comments

--- src/middlewares/authenticateUserMiddleware.js
+++ src/middlewares/authenticateUserMiddleware.js
@@ -1,22 +1,33 @@
 const auth = require('basic-auth');
 const User = require('./../db/models/User');
 
+/**
+ * Module Authenticates the user against the DB
+ * @param req - Request
+ * @param res - Response
+ * @param next - next()
+ * @returns {*}
+ */
 module.exports = (req, res, next) => {
+  /**
+   * Reads the name & password from the Authentication Header
+   * @type {never|auth.BasicAuthResult}
+   */
   const credentials = auth(req);
 
   if (!credentials) {
-    const error = new Error('Credentials missing!');
+    const error = new Error('Credentials missing.');
     error.status = 400;
     return next(error);
   } else {
-    console.log('User: ', User);
+    /**
+     * Sends the name & password to check if they are the same
+     */
     User.auth(credentials.name, credentials.pass, (error, user) => {
       if (error) {
         console.error(error);
         return next(error);
       }
-
-      console.log('setting req.currentAuthUser: ', user);
 
       req.currentAuthUser = user;
       return next();
